c456557ecc48dead3f4fb09aea02ad789e39325c
hallisonassuncao <hallison_assuncao@outlook.com>
2025-12-19 08:52:41 -0300

    Ajustes

--- src/backend/daos/pratoDAO.js
+++
@@ -1,9 +0,0 @@
-const Prato = require('../models/Prato');
-
-module.exports = {
-  list: () => Prato.find(),
-  get: (id) => Prato.findById(id),
-  create: (data) => Prato.create(data),
-  update: (id, data) => Prato.findByIdAndUpdate(id, data, { new: true }),
-  remove: (id) => Prato.findByIdAndDelete(id),
-};
